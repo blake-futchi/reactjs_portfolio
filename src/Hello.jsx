f5088a61cfd80516ef6c4a8a9afb432cdad0b731
Blake <blake@futchi.com>
2020-02-19 13:17:07 +0100

    added cards to services , need to update with relevant content, added hover effect, attempting to create hero image in root

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -1,16 +1,18 @@
 import React from "react";
+import HeroImage from "./HeroImage";
 
 const Hello = () => {
-  return (
+  return ( 
     <div class="hero-image">
-  <div class="hero-text">
-    <h1>I am Blake Erickson</h1>
-    <h4>Entrepreneur | Junior developer | freelancer</h4>
-    <p class="mt-4"> Climber | Surfer | Gymnast | Footballer | Adventure Seeker</p>
-    <div class="item"><a class="ui button" onClick={event =>  window.location.href='/About'}>Read More</a>
-    <a class="ui primary button" href="mailto:blake@futchi.com" subject="Hey Blake! Let's work together">Contact Me</a></div>
-  </div>
-</div>
+    <img src={HeroImage} alt="no image found"/>
+      <div class="hero-text">
+        <h1>I am Blake Erickson</h1>
+        <h4>Entrepreneur | Junior developer | freelancer</h4>
+        <p class="mt-4"> Climber | Surfer | Gymnast | Footballer | Adventure Seeker</p>
+        <div class="item"><a class="ui button" onClick={event =>  window.location.href='/About'}>Read More</a>
+        <a class="ui primary button" href="mailto:blake@futchi.com" subject="Hey Blake! Let's work together">Contact Me</a></div>
+      </div>
+    </div>
   );
 }
 export default Hello;
