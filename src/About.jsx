9038bbe50ee4ea1f167ed9d5295b09c1618a0c6a
Blake <blake@futchi.com>
2020-02-20 12:03:38 +0100

    opacity to hero image

--- src/About.jsx
+++ src/About.jsx
@@ -3,7 +3,7 @@ import { UndrawDesignerLife } from "react-undraw-illustrations";
 
 const About = () => {
   return (
-    <div className="ui main container">
+    <div className="ui main container container-about">
       <div className="hero-image hero-about" style={{ background: 'url("./src/img/me.jpg")', backgroundSize: 'cover', height: '100vh'}}>
   
       </div>
