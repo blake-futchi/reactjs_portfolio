81defd68cdf47a7030a9b0f8b3015e65fc537d7b
Blake <blake@futchi.com>
2020-02-22 12:05:32 +0100

    rewrote all remaining tests for site, passing green

--- cypress/integration/userCanSeeListOfProjects.feature.js
+++ cypress/integration/userCanSeeListOfProjects.feature.js
@@ -5,25 +5,29 @@ describe('User can see list of projects', () => {
   })
 
   it('displays first project', () => {
-    cy.get('.case-study').within(() => {
+    cy.get('.super').within(() => {
       cy.get('.info-box').should('contain', 'Cofounded and launched Futchi');
       cy.get('.ui.red.basic.button').should('contain', 'Go to futchi.com');
     })
   });
 
   it('displays second project', () => {
-    cy.get('.info-box.box-1').within(() => {
-      cy.get(('.image')).should('exist');
-      cy.get('.ui.header').should('contain', 'UI Design');
-      cy.get('.description').should('contain', 'Designing user interfaces is fun. I want to learn more about that...');
+    cy.get('.super').within(() => {
+      cy.get('.info-box').should('contain', 'Developed and executed');
+      cy.get('.ui.red.basic.button').should('contain', 'Go to fast.com');
     })
   });
 
   it('displays third project', () => {
-    cy.get('.info-box.box-2').within(() => {
-      cy.get(('.image')).should('exist');
-      cy.get('.ui.header').should('contain', 'Mobile UX');
-      cy.get('.description').should('contain', "I like to design for the mobile platform. The challenges to build UI's for smartphones is challenging but extremely rewarding.");
+    cy.get('.super').within(() => {
+      cy.get('.info-box').should('contain', 'Directed experience mapping');
+      cy.get('.ui.red.basic.button').should('contain', 'Go to wsderm.com');
+    })
+  });  
+  it('displays fourth project', () => {
+    cy.get('.super').within(() => {
+      cy.get('.info-box').should('contain', 'Sometimes a new perspective');
+      cy.get('.ui.red.basic.button').should('contain', 'Follow my');
     })
   });  
 });
