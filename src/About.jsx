711dbf7d357d902d389d7429896c332afd66298e
Blake <blake@futchi.com>
2020-02-23 14:02:46 +0100

    removed json for about and updated css for div classes

--- src/About.jsx
+++ src/About.jsx
@@ -1,46 +1,18 @@
-import React, { Component } from "react";
-import axios from "axios";
+import React from "react"
 
-class About extends Component {
-  state = {
-    projects: [
-    ]
-  };
-
-  render() {
-    const projects = this.state.projects;
-    let projectsList;
-
-    if (projects.length > 0) {
-      projectsList = projects.map(project => {
-        return (
-          <div className="ui main container container-about-text">
-            <div className="ui header" id="about-text">{project.description}</div>
-          </div>
-        );
-      });
-    }
-
-    return (
+const About = () => {
+  return (
       <div className="ui main container">
-        <div className="ui main container container-about">
-      <div className="hero-image hero-about" style={{ background: 'url("./src/img/me.jpg")', backgroundSize: 'cover', height: '100vh'}}>
-        {projectsList}
+        <div className="ui main container container about">
+          <div className="hero-image hero-about" style={{ background: 'url("./src/img/me.jpg")', backgroundSize: 'cover', height: '100vh'}}>
+            <div className="ui main container container-about-text">
+              No siempre bebo cerveza, pero cuando lo hago, bebo Dos Equis...
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-        
-      </div>
-    );
+  );
   }
 
-  componentDidMount() {
-    axios.get('./src/data/projects.json')
-      .then(response => {
-        this.setState({
-          projects: response.data
-        })
-      })
-  }
-}
-
+          
 export default About;
